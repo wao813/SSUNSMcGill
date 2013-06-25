5c586a7a84332318eac7f1eacc08011e2d09656a
wao813 <wao813@gmail.com>
2013-06-25 16:30:14 -0700

    Initial Commit

---
+++ SSUNS/SUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SUAppDelegate.h
+//  SSUNS
+//
+//  Created by James on 2013-06-25.
+//  Copyright (c) 2013 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SSUNS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSUNS
+//
+//  Created by James on 2013-06-25.
+//  Copyright (c) 2013 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SUAppDelegate class]));
+    }
+}
